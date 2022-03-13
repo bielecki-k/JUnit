1f0aecc0a95cb34ded142fc52c7e1bac8848f961
Kn Bielecki <kn-bielecki@protonmail.com>
2022-03-13 19:13:35 +0100

    gwt

--- currentMeterTests/src/test/java/electricity/ElectricityMeterTest.java
+++ currentMeterTests/src/test/java/electricity/ElectricityMeterTest.java
@@ -7,9 +7,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class ElectricityMeterTest {
 
     @Test
-    void addKwh() {
+    void addKwh_newMeter_properAddition() {
+        //given
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        //when
+        electricityMeter.addKwh(1);
+        //then
+        assertEquals(1, electricityMeter.getKwh());
+    }
+
+    @Test
+    void addKwh_newMeter2_properAddition() {
         ElectricityMeter electricityMeter = new ElectricityMeter();
         electricityMeter.addKwh(1);
-        assertEquals(1, electricityMeter.getKwh());
+        electricityMeter.addKwh(4);
+        assertEquals(5, electricityMeter.getKwh());
+    }
+
+
+    @Test()
+    void getHowMoreExpensiveNormalIs() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.setCentsForKwh(90);
+        assertThrows(ArithmeticException.class, electricityMeter::getHowMoreExpensiveNormalIs);
     }
 }
